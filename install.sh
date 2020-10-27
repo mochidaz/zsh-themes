446b1054aed69c3f34a77a416bf5615e5c4131b7
Rahman <rahmanhakim2435@gmail.com>
2020-10-28 05:20:39 +0700

    Fixed install

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
+#! /usr/bin/env bash
+
 DIR="/home/$USER/.oh-my-zsh/"
 
 if [ -d "$DIR" ]; then
 	echo "Installing..."
 	cp ./themes/* $DIR"themes"
 	echo "Installation success"
-	read -r p "For best experience, do you want to install Powerline fonts? [Y/n]" response
-	if [[ "$response" =~ ^([yY])$ ]]; then
+	read -r -p "For the better experience, do you want to install Powerline fonts? [Y/n] " response
+	if [[ "$response" =~ ^([yY]) ]]; then
 			echo "Cloning into powerline fonts..."
 			git clone https://github.com/powerline/fonts /tmp/powerline-fonts
 			echo "Installing fonts..."
 			/tmp/powerline-fonts/install.sh
-			echo "Install done. For the best experience, it is recommended to use Inconsolata for powerline font. Thanks for installing!"
+			echo " "
+			echo "Install done. For the best experience, it is recommended to use one of the powerline-patched fonts. Thanks for installing!"
 	else
+		echo "Thanks for installing!"
 		echo "Exitting..."
+	fi
 
 else
 	echo "Please Install oh-my-zsh first. If you have installed it, but this installation still fails, then .oh-my-zsh directory isn't set correctly"
